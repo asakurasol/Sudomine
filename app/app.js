6855d972d3157759fa642b01e292b2421701c150
Derek Wu <Derek@Dereks-MacBook-Air.local>
2014-10-06 08:43:34 -0700

    changed routing

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  //'myApp.view2',
+  //'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
